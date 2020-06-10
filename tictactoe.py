2277e21c5604a08ac28119a5eea3bec2044a0a89
SebastianBravo <bravos.js@javeriana.edu.co>
2020-06-10 18:04:46 -0500

    Commented Code

--- tictactoe.py
+++ tictactoe.py
@@ -23,12 +23,15 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
+    # Checks if the game is over
     if terminal(board):
         return "Game is already over"
 
+    # Variables that keep track of number of X and O
     num_x = 0
     num_o = 0
 
+    # Counts the number of X and O in the board
     for row in board:
     	for cell in row:
     		if cell == X:
@@ -36,6 +39,7 @@ def player(board):
     		elif cell == O:
     			num_o += 1
 
+    # Decides the next turn
     if num_x > num_o:
     	return O
     else:
@@ -46,11 +50,14 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
+    # Checks if the game is over
     if terminal(board):
         return "Game is already over"
 
+    # Initialize an empty actions set
     possible_actions = set()
 
+    # Add all the possible actions to the set
     for i in range(len(board)):
     	for j in range(len(board[i])):
             cell = board[i][j]
@@ -64,11 +71,14 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    # Variables that keep the coordenates of the action
     i = action[0]
     j = action[1]
 
+    # Create a copy of the orginal board avoiding any alteration. 
     result_board = copy.deepcopy(board)
 
+    # Checks wether the action is posssible or no
     if board[i][j] == EMPTY:
         result_board[i][j] = player(board)
         return result_board
@@ -80,20 +90,25 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+    # Variables that represents a win
     x_win = [X, X, X]
     o_win = [O, O, O]
 
+    # Initialize lists for the rows, columns and diagonals in the board
     diagonals = [[board[0][0], board[1][1], board[2][2]],
                 [board[2][0], board[1][1], board[0][2]]]
     rows = copy.deepcopy(board)
     columns = []
 
+    # Looks for all the columns and add the to the list
     for num_col in range(3):
         column = [row[num_col] for row in board]
         columns.append(column)
 
+    # Creates a list with all the possible rows made of 3 items
     three_in_a_row = diagonals + columns + rows
 
+    # Checks who is the winner if there's one
     if x_win in three_in_a_row:
         return X
     elif o_win in three_in_a_row:
@@ -106,8 +121,10 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """ 
+    # Checks if there is any empty space in the board
     full = not any(EMPTY in row for row in board)
 
+    # Checks if the game is over
     if winner(board) or full:
         return True
     else:
@@ -130,14 +147,17 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    #best explored option along path to root for maximizer 
+    # Best explored action along path to root for maximizer 
     alpha = -math.inf
 
-    #best explored option along path to root for minimizer
+    # Best explored action along path to root for minimizer
     beta = math.inf
 
+    # Initialize an empty dictionary to keep track the values and 
+    # the actions that produced those values
     values_actions = {}
 
+    # Find the optimal action por the current player
     if player(board) == X:
         for action in actions(board):
             v = min_value(result(board, action), alpha, beta)
@@ -158,7 +178,9 @@ def minimax(board):
 
 
 def max_value(board, alpha, beta):
-
+    """
+    Returns the max value associated to a board 
+    """
     if terminal(board):
         return utility(board)
 
@@ -174,6 +196,9 @@ def max_value(board, alpha, beta):
     return v
 
 def min_value(board, alpha, beta):
+    """
+    Returns the min value associated to a board 
+    """
     if terminal(board):
         return utility(board)
 
